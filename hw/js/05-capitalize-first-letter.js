e64fc5566d42b19cb11d1541505139e7fb8402a9
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-04-29 18:21:31 -0700

    05 capitalize first letter of every word in a sentence and return a new sentence.

--- hw/js/05-capitalize-first-letter.js
+++ hw/js/05-capitalize-first-letter.js
@@ -3,13 +3,23 @@ Write a JavaScript function that accepts a string as a parameter and converts th
 of each word of the string in upper case.
 */
 
+// Runtime O(3N) => O(N), Space O(2N) => O(N).
 function capsFirstChar(str) {
-    let strArr = str.split(' ');
-    for (let i = 0; i < strArr.length; i++) {
+    if (typeof str !== 'string') return 'Invalid input';
+    else if (str === '') return '';
+    let strArr = str.split(' ');        // O(N) runtime and O(N) space where N is the amount of words
+    for (let i = 0; i < strArr.length; i++) { // O(N) runtime where N is the amount of words.
         let word = strArr[i];
-        strArr[i] = word[0].toUpperCase() + word.slice(1);
+        strArr[i] = word[0].toUpperCase() + word.slice(1); // O(1) space for variable reassignment
     }
-    return strArr.join(' ');
+    return strArr.join(' ');    // O(N) runtime where N is the amount of words, O(N) space for creating a new sentence
 }
 
-console.log(capsFirstChar())
+console.log(capsFirstChar([1,2,3])); // 'Invalid input.'
+console.log(capsFirstChar({'a': true, 'b': 1})); // 'Invalid input.'
+console.log(capsFirstChar()); // 'Invalid input.'
+console.log(capsFirstChar(null)); // 'Invalid input.'
+console.log(capsFirstChar(undefined)); // 'Invalid input'
+console.log(capsFirstChar(123)); // 'Invalid input.'
+console.log(capsFirstChar('the quick brown fox')); // 'The Quick Brown Fox
+console.log(capsFirstChar('')); // ''
