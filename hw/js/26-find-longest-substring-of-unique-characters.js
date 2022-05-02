78180a1e1a9988b4d67ed6aaecbf3ec8c2ccb371
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-05-01 19:07:16 -0700

    find longest substring of unique characters in a string.

--- hw/js/26-find-longest-substring-of-unique-characters.js
+++ hw/js/26-find-longest-substring-of-unique-characters.js
@@ -8,65 +8,70 @@ Q:
 - What if string input is not a string? I'd return 'Invalid input.'
 - What if input is an empty string? I'd return empty string;
 - Is it case sensitive? Is 'A' = 'a'? I'd treat it case sensitive: 'A' !== 'a'
+- If there are two or more substrings with the same maximum length, do we return all? or the first instance? or the last instance?
+    I'd return the first instance
 */
 
+/* WORKS, with POJO as a set
 function longestSubstringOfUniqueC(str) {
     if (typeof str !== 'string') return 'Invalid input.';
+    if (str === '') return str;
     let set = {};
-    let substring = '';
-    let max = {length: 0, strg: ''};
+    set[str[0]] = true;
+    let longestSub = str[0];
+    let currentSub = str[0];
 
-    for (let i = 0; i < str.length; i++) { // <= because we need to run one more loop to check the last substring we saw
+    for (let i = 1; i < str.length; i++) { // <= because we need to run one more loop to check the last substring we saw
         let char = str[i];
         // if set has this character, OR we reached the end of string, check it agains our max and store it if it fits requirement
         if (set[char]) {
-            if (substring.length > max.length) {
-                max.strg = substring;
-                max.length = substring.length;
-                substring = char;
-                set = {};
-                set[char] = true;
-            }
-        } else {
+            set = {};
             set[char] = true;
-            substring += char;
+            if (currentSub.length > longestSub.length) {
+                longestSub = currentSub;
+            }
+            currentSub = char;
+        } else { // if char is not a key in set
+            set[char] = true;
+            currentSub += char;
         }
     }
-    return substring.length > max.length ? substring : max.strg;
-}
+    return currentSub.length > longestSub.length ? currentSub : longestSub;
+} */
 
-// TEST
-console.log(longestSubstringOfUniqueC('abcabcdabcde')); // 'abcde
-console.log(longestSubstringOfUniqueC('abcabcdAbcde')); // 'abcdA // first instance found
-
-console.log(40, longestSubstringOfUniqueC('The quick brown fox jump over'))
-
-
-// console.log(longestSubstringOfUniqueC(123)); // 'Invalid input.'
-// console.log(longestSubstringOfUniqueC('')); // ''
-
-
-/*
+// WORKS. With built in Set
 function longestSubstringOfUniqueC(str) {
     if (typeof str !== 'string') return 'Invalid input.';
-    let set = new Set();
-    let substring = '';
-    let max = {length: 0, strg: ''};
+    if (str === '') return str;
+    let set = new Set(str[0]);
+    let longestSub = str[0];
+    let currentSub = str[0];
 
-    for (let i = 0; i < str.length; i++) { // <= because we need to run one more loop to check the last substring we saw
+    for (let i = 1; i < str.length; i++) { // <= because we need to run one more loop to check the last substring we saw
         let char = str[i];
         // if set has this character, OR we reached the end of string, check it agains our max and store it if it fits requirement
         if (set.has(char)) {
-            if (substring.length > max.length) {
-                max.strg = substring;
-                max.length = substring.length;
-                substring = char;
-                set = new Set(char);
+            set = new Set(char);
+            if (currentSub.length > longestSub.length) {
+                longestSub = currentSub;
             }
+            currentSub = char;
         } else {
             set.add(char);
-            substring += char;
+            currentSub += char;
         }
     }
-    return substring.length > max.length ? substring : max.strg;
-} */
+    return currentSub.length > longestSub.length ? currentSub : longestSub;
+}
+
+
+// TEST
+console.log(longestSubstringOfUniqueC('abcabcdabcde')); // 'abcde
+console.log(longestSubstringOfUniqueC('abcabcdAbcde')); // 'abcdA // first instance found, 'A' !== 'a'
+console.log(longestSubstringOfUniqueC('The quick brown fox jump over')); // 'The quick'
+console.log(longestSubstringOfUniqueC(123)); // 'Invalid input.'
+console.log(longestSubstringOfUniqueC('')); // ''
+console.log(longestSubstringOfUniqueC()); // 'Invalid input.'
+console.log(longestSubstringOfUniqueC(null)); // 'Invalid input.'
+console.log(longestSubstringOfUniqueC(undefined)); // 'Invalid input.'
+console.log(longestSubstringOfUniqueC(['abracadabra'])); // 'Invalid input.'
