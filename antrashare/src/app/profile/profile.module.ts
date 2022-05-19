6101f59aefc7212ba6f162c4f8d59791adb1959a
max <david_zhangxp@yahoo.com>
2022-05-19 19:24:13 -0400

    create 6 modules for team project

---
+++ antrashare/src/app/profile/profile.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProfileComponent } from './profile.component';
+
+@NgModule({
+  declarations: [ProfileComponent],
+  imports: [CommonModule],
+  exports: [ProfileComponent],
+})
+export class ProfileModule {}
---
+++ antrashare/src/app/register/register.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './register.component';
+
+@NgModule({
+  declarations: [RegisterComponent],
+  imports: [CommonModule],
+  exports: [RegisterComponent],
+})
+export class RegisterModule {}
