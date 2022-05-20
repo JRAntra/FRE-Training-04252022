02d200cfb58d92cd12920353f3c8bf36db74c00b
KikiGuoguo <56179585+guoguorani@users.noreply.github.com>
2022-05-19 23:32:30 -0700

    add first three modules

--- theProject/src/app/app.module.ts
+++ theProject/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyprofileComponent } from './myprofile/myprofile.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyprofileComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
