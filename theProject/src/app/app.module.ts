9dce87a07260e24f86035fd602b513f5dd0b2ca7
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-05-24 10:15:48 -0700

    created setting.module and setting-routing.module, export it, and modified app.module accordingly.

--- theProject/src/app/app.module.ts
+++ theProject/src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginModule } from './login/login.module';
 import { RegisterModule } from './register/register.module';
+import { SettingModule } from './setting/setting.module';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { FeedComponent } from './feed/feed.component';
 import { AdminComponent } from './admin/admin.component';
-import { SettingComponent } from './setting/setting.component';
 import { StoryComponent } from './feed/story-list/story/story.component';
 import { PostComponent } from './feed/post/post.component';
 import { CommentComponent } from './feed/story-list/story/comment/comment.component';
@@ -22,7 +22,6 @@ import { StoryListComponent } from './feed/story-list/story-list.component';
     ProfileComponent,
     FeedComponent,
     AdminComponent,
-    SettingComponent,
     StoryComponent,
     PostComponent,
     CommentComponent,
@@ -32,7 +31,8 @@ import { StoryListComponent } from './feed/story-list/story-list.component';
     BrowserModule,
     AppRoutingModule,
     LoginModule,
-    RegisterModule
+    RegisterModule,
+    SettingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
