74e8db2e360910c46999e88935381442bbdaec2b
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-05-24 13:25:33 -0700

    added admin-routing.module, admin.module, feed-routing.module, feed-module, login-routing.module, login.module, profile-routing.module, profild.module, register-routing.module, register.module, setting-routing.module, setting.module, and also amended declarations and imports within app.module accordingly.

--- theProject/src/app/app.module.ts
+++ theProject/src/app/app.module.ts
@@ -2,36 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { FeedComponent } from './feed/feed.component';
-import { AdminComponent } from './admin/admin.component';
-import { SettingComponent } from './setting/setting.component';
-import { StoryComponent } from './feed/story-list/story/story.component';
-import { PostComponent } from './feed/post/post.component';
-import { CommentComponent } from './feed/story-list/story/comment/comment.component';
-import { StoryListComponent } from './feed/story-list/story-list.component';
+import { LoginModule } from './login/login.module';
+import { RegisterModule } from './register/register.module';
+import { SettingModule } from './setting/setting.module';
+import { ProfileModule } from './profile/profile.module';
+import { AdminModule } from './admin/admin.module';
+import { FeedModule } from './feed/feed.module';
 
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProfileComponent,
-    LoginComponent,
-    FeedComponent,
-    AdminComponent,
-    SettingComponent,
-    RegisterComponent,
-    StoryComponent,
-    PostComponent,
-    CommentComponent,
-    StoryListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginModule,
+    RegisterModule,
+    SettingModule,
+    ProfileModule,
+    AdminModule,
+    FeedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
