1e317cea9aeca268a877be26f0f86b2afc4e372b
SunnySun0813 <71992143+SunnySun0813@users.noreply.github.com>
2022-05-25 10:04:51 +0800

    nav

--- antrashare/src/app/app.module.ts
+++ antrashare/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { LoginModule } from './login/login.module';
 import { RegisterModule } from './register/register.module';
@@ -8,22 +7,23 @@ import { SettingModule } from './setting/setting.module';
 import { ProfileModule } from './profile/profile.module';
 import { AdminModule } from './admin/admin.module';
 import { FeedModule } from './feed/feed.module';
-
+import { NavigationComponent } from './navigation/navigation.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
+    NavigationComponent,
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
     LoginModule,
     RegisterModule,
     SettingModule,
     ProfileModule,
     AdminModule,
-    FeedModule
+    FeedModule,
+    AppRoutingModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
