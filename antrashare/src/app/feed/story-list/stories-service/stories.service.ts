c23200840cf9e40da91ad901938691b3a0df14cd
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-05-31 23:26:26 -0700

    moving stories-service/ into story-list/.

--- antrashare/src/app/feed/story-list/stories-service/stories.service.ts
+++ antrashare/src/app/feed/story-list/stories-service/stories.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { News } from '../../shared/models/News';
+import { News } from '../../../shared/models/News';
 
 @Injectable({
   providedIn: 'root'
--- antrashare/src/app/feed/story-list/story-list.component.ts
+++ antrashare/src/app/feed/story-list/story-list.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { News } from '../../shared/models/News';
+import { StoriesService } from './stories-service/stories.service';
 
 @Component({
   selector: 'app-story-list',
   templateUrl: './story-list.component.html',
   styleUrls: ['./story-list.component.sass']
 })
+
 export class StoryListComponent implements OnInit {
 
   constructor() { }
