463195d58802f857f122370ec19075a98c7bf279
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-06-07 11:01:30 -0700

    implemented validators for username and password: required, minLength, maxLength. Need more time for the regex filter validation for password input.

--- antrashare/src/app/profile/profile.component.ts
+++ antrashare/src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { User } from '../shared/models/User';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.sass']
 })
 export class ProfileComponent implements OnInit {
+  errorMsg: string[] = [];
+  @Input() user!: User;
+  hide: boolean = true;
 
   constructor() { }
 
+  inputForm = new FormGroup({
+    username: new FormControl(
+      '',
+      [ Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(12) ]
+    ),
+    password: new FormControl(
+      '',
+      [ Validators.minLength(5) ]
+    ),
+    gender: new FormControl(''),
+    age: new FormControl(''),
+    phone: new FormControl('')
+  })
+
+
+  get username() {
+    return this.inputForm.get('username');
+  }
+
+  get age() {
+    return this.inputForm.get('age');
+  }
+
+  get gender() {
+    return this.inputForm.get('gender');
+  }
+
+  get password() {
+    return this.inputForm.get('password');
+  }
+
+  onSave() {}
+
   ngOnInit(): void {
+
+    this.inputForm = new FormGroup({
+      username: new FormControl(
+        '',
+        [ Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(12) ]
+      ),
+      password: new FormControl(
+        '',
+        [ Validators.required,
+          Validators.minLength(5) ]
+      ),
+      gender: new FormControl(''),
+      age: new FormControl(''),
+      phone: new FormControl('')
+    })
   }
 
 }
--- antrashare/src/app/profile/profile.module.ts
+++ antrashare/src/app/profile/profile.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { ProfileRoutingModule } from './profile-routing.module';
-
 import { ProfileComponent } from './profile.component';
 
 @NgModule({
@@ -11,7 +10,8 @@ import { ProfileComponent } from './profile.component';
     ],
     imports: [
         CommonModule,
-        ProfileRoutingModule
+        ProfileRoutingModule,
+        ReactiveFormsModule
     ]
 })
 
