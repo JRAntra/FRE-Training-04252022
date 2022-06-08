07598b381e3bff8c13ea21a64a5c655544631441
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-06-07 18:09:49 -0700

    Finished impementing validators for username and password. onSave() function intentionally left blank for further instruction.

--- antrashare/src/app/profile/profile.component.ts
+++ antrashare/src/app/profile/profile.component.ts
@@ -1,17 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+    Component
+  , Input
+  , OnInit
+} from '@angular/core';
 
-import { User } from '../shared/models/User';
+import {
+    FormGroup
+  , FormControl
+  , AbstractControl
+  , ValidatorFn
+  , Validators
+  , ValidationErrors
+} from '@angular/forms';
+
+import { User, dummyUser } from '../shared/models/User';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.sass']
 })
+
 export class ProfileComponent implements OnInit {
+  avatar: string = "https://d7hftxdivxxvm.cloudfront.net/?resize_to=fit&width=800&height=800&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2F_DogUIUeT1Ke3UvxGQbPGA%2Fnormalized.jpg"
   errorMsg: string[] = [];
-  @Input() user!: User;
+  @Input() user: User = dummyUser;
   hide: boolean = true;
+
+  // at least 1 uppercase, 1 lowercase, 1 special char
+  regexFilter: RegExp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[#?!@$%^&*-])$/
 
   constructor() { }
 
@@ -24,10 +41,13 @@ export class ProfileComponent implements OnInit {
     ),
     password: new FormControl(
       '',
-      [ Validators.minLength(5) ]
+      [ Validators.minLength(5),
+        passwordValidator(this.regexFilter)
+      ]
     ),
     gender: new FormControl(''),
     age: new FormControl(''),
+    email: new FormControl(''),
     phone: new FormControl('')
   })
 
@@ -36,19 +56,17 @@ export class ProfileComponent implements OnInit {
     return this.inputForm.get('username');
   }
 
-  get age() {
-    return this.inputForm.get('age');
-  }
-
-  get gender() {
-    return this.inputForm.get('gender');
-  }
-
   get password() {
     return this.inputForm.get('password');
   }
 
-  onSave() {}
+  toggleShow() {
+    this.hide = !this.hide;
+  }
+
+  onSave() {
+    console.log('onSave()', this.inputForm.value);
+  }
 
   ngOnInit(): void {
 
@@ -62,12 +80,31 @@ export class ProfileComponent implements OnInit {
       password: new FormControl(
         '',
         [ Validators.required,
-          Validators.minLength(5) ]
+          Validators.minLength(5),
+          passwordValidator(this.regexFilter) ]
       ),
       gender: new FormControl(''),
       age: new FormControl(''),
+      email: new FormControl(''),
       phone: new FormControl('')
     })
   }
 
 }
+
+// Directive for password characters validator starts here.
+export function passwordValidator(filter: RegExp): ValidatorFn {
+  console.log('passwordValidator() taking', filter)
+  return (control: AbstractControl): ValidationErrors | null => {
+    console.log('anonymousFn() taking', control)
+    const isValid = filter.test(control.value);
+    const result = isValid ? {validPassword: { value: control.value}}: null;
+    console.log('compare result isValid, ', isValid, result)
+    return result;
+  }
+}
+// Directive for password characters validator starts here.
+
+// Ref.: https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+// at least 8 chars, 1 uppercase, 1 lowercase, 1 number, 1 special char
+// /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
