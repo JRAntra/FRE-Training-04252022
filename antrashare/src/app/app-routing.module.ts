ab56a2effc89fc51f9f536f952ad2f70294a0746
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-06-12 09:20:13 -0700

    edit app-routing.module.ts by adding lazy loading to the routes array for six modules: feed, profile, admin, setting, register, and login, also a redirect from root url to login.

--- antrashare/src/app/app-routing.module.ts
+++ antrashare/src/app/app-routing.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { FeedComponent } from './feed/feed.component';
+import { LoginComponent } from './login/login.component';
 import { LoginModule } from './login/login.module';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+import { SettingComponent } from './setting/setting.component';
 import { ErrorComponent } from './shared/error/error.component';
 // import { HomeComponent } from '.home/home.component';
 
@@ -8,6 +14,43 @@ const routes: Routes = [
   // { path: '', component: LoginModule }, //don't know why iss not working
   { path: '**', redirectTo: 'login'}
   // { path: '/text/:id', redirectTo:'', pathMatch:'prefix' }
+
+  {
+    path: 'feed',
+    component: FeedComponent,
+    loadChildren: () => import('src/app/feed/feed.module').then((m) => m.FeedModule)
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    loadChildren: () => import('src/app/profile/profile.module').then((m) => m.ProfileModule)
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    loadChildren: () => import('src/app/admin/admin.module').then((m) => m.AdminModule)
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    loadChildren: () => import('src/app/login/login.module').then((m) => m.LoginModule)
+  },
+  {
+    path: 'setting',
+    component: SettingComponent,
+    loadChildren: () => import('src/app/setting/setting.module').then((m) => m.SettingModule)
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    loadChildren: () => import('src/app/register/register.module').then((m) => m.RegisterModule)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
+
 ];
 
 @NgModule({
