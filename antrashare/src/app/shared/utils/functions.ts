acbad91a4392b88e1c1da3289306f3a9118ca0bc
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-06-14 23:15:24 -0700

    adding default value 30 minutes to the setExpireInUnix function.

--- antrashare/src/app/shared/utils/functions.ts
+++ antrashare/src/app/shared/utils/functions.ts
@@ -1,5 +1,5 @@
-// method to set expiration date in unix form (typeof number)
-export const setExpireInUnix: Function = (seconds: number): number => {
+// method to set expiration date in unix form (typeof number) with default value 30 minutes
+export const setExpireInUnix: Function = (seconds: number = 30 * 60): number => {
     const now: string = new Date().toUTCString()
     ,   unixNow: number = Date.parse(now)
     ,   unixExp: number = unixNow + seconds;
