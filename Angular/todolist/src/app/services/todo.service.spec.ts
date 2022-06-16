cfd3b62ef00b4a9eaaac3064205c0e06bd948592
show3567 <ydong011@fiu.edu>
2022-06-16 15:38:10 -0500

    update

--- Angular/todolist/src/app/services/todo.service.spec.ts
+++ Angular/todolist/src/app/services/todo.service.spec.ts
@@ -1,16 +1,50 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
-// import { TodoService } from './todo.service';
+import { TodoService } from './todo.service';
 
-// describe('TodoService', () => {
-//   let service: TodoService;
+describe('TodoService', () => {
+  let service: TodoService;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(TodoService);
-//   });
+  const testTodolist = [
+    {
+      userId: 1,
+      id: 1,
+      title: 'delectus aut autem',
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: 'quis ut nam facilis et officia qui',
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 3,
+      title: 'fugiat veniam minus',
+      completed: false,
+    },
+  ];
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    service = TestBed.inject(TodoService);
+  });
+
+  it('should get todos by trigger the getTodos method', () => {
+    spyOn(service, 'getTodos').withArgs().and.returnValues(of(testTodolist));
+
+    service.getTodos().subscribe((data) => {
+      expect(data).toEqual(testTodolist);
+    });
+    expect(service.getTodos).toHaveBeenCalledTimes(1);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
--- Angular/todolist/src/app/todo-item/todo-item.component.spec.ts
+++ Angular/todolist/src/app/todo-item/todo-item.component.spec.ts
@@ -1,25 +1,56 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { TodoItemComponent } from './todo-item.component';
+import { TodoItemComponent } from './todo-item.component';
 
-// describe('TodoItemComponent', () => {
-//   let component: TodoItemComponent;
-//   let fixture: ComponentFixture<TodoItemComponent>;
+@Component({
+  selector: 'parent-component',
+  template: `<app-todo-item [item]="todo"></app-todo-item>`,
+})
+class ParentComponent {
+  todo = {
+    userId: 1,
+    id: 2,
+    title: 'quis ut nam facilis et officia qui',
+    completed: false,
+  };
+}
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ TodoItemComponent ]
-//     })
-//     .compileComponents();
-//   });
+describe('TodoItemComponent', () => {
+  let component: TodoItemComponent;
+  let fixture: ComponentFixture<TodoItemComponent>;
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(TodoItemComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  let itemcomponent: TodoItemComponent;
+  let parentfixture: ComponentFixture<ParentComponent>;
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TodoItemComponent, ParentComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TodoItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  beforeEach(() => {
+    parentfixture = TestBed.createComponent(ParentComponent);
+    itemcomponent = parentfixture.debugElement.children[0].componentInstance;
+    parentfixture.detectChanges();
+  });
+
+  it('should get data from the parent', () => {
+    const todo = {
+      userId: 1,
+      id: 2,
+      title: 'quis ut nam facilis et officia qui',
+      completed: false,
+    };
+
+    expect(itemcomponent.todo).toEqual(todo);
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
